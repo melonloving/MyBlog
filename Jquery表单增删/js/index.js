62efd3404bb20611d8b23232329158099dc49321
melonloving <1071512882@qq.com>
2017-06-13 10:22:29 +0800

    双向数据绑定
    json数据绑定

---
+++ Jquery表单增删/js/index.js
@@ -0,0 +1,17 @@
+function getResult()
+{
+	
+	
+	
+	
+	
+	
+	
+	
+//	var result={};
+//	result.name=$.trim($('input[name="Name"]').val());
+//	result.workID=$('input[name="WorkID"]').val();
+//	result.startdate=$('input[name="StartDate"]').val();
+//	result.enddate=$('input[name="EndDate"]').val();
+//	return result;
+}
